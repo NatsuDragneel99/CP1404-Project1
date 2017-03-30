bc7cca2decc78a76eb89ae70e68a2ddc9254ef81
BroderickThomsen <broderickwsthomsen@gmail.com>
2017-03-30 21:52:42 +1000

    Add load_book function and quit program feature

--- assignment1.py
+++ assignment1.py
@@ -1,10 +1,28 @@
 """
-Replace the contents of this module docstring with your own details
+Broderick Thomsen, 29th March
+Project details:
+
+https://github.com/CP1404-2017-1/a1-BroderickWST
 """
 
 
 def main():
-    print("Reading List 1.0 - by <Your Name>")
+    print("Reading List 1.0 - by Broderick Thomsen")
+    file = open("temp.csv", "r+")
+    book_list = []
+    print("{} books loaded from {}".format(len(load_book(book_list, file)), file.name))
+    program_loop = 1  # This method of looping is subject to change
+    while program_loop == 1:
+        print("Menu:\nR - List required books\nC - List completed books\nA - Add new book\nM - Mark a book as completed\nQ - Quit")
+        menu_input = str(input(">>> "))
+        if menu_input == "q" or menu_input == "Q":
+            program_loop = 0
+
+
+def load_book(book_list, file):  # TODO: rename parameter
+    for line in file.readlines():
+        book_list.append(line.split(","))
+    return book_list
 
 
 if __name__ == '__main__':
