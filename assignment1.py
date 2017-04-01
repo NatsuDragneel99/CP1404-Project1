fd28034b2d8c861fb2f7420afdbfd71b0b6760f0
BroderickThomsen <broderickwsthomsen@gmail.com>
2017-04-01 14:48:03 +1000

    Add page_sum() and completed books in display_book

--- assignment1.py
+++ assignment1.py
@@ -15,19 +15,14 @@ def main():
     while program_loop != 0:
         print("Menu:\nR - List required books\nC - List completed books\nA - Add new book\nM - Mark a book as completed\nQ - Quit")
         menu_input = str(input(">>> "))
-        if menu_input == "r" or menu_input == "R": # TODO: Error checking if books aren't there. Print "Required Books" if there, "None" if not.
-            page_sum = 0 #gotta be a better way to count pages - function?
-            required_books = 0 # this could be in a list - make required reading into a list and return that from display books so can be used for "Required Books" and to easily count how many required books there are. You can return it back to the main list when done so changes are always accounted for, or maybe just do that when marking/adding a book.
-            for selection, i in enumerate(book_list):
-                display_book(i, selection)
-                if i[-1] == "r\n": # this is a mess
-                    page_sum += int(i[2])
-                    required_books += 1
-            print ("Total pages for {} books: {}".format(required_books,page_sum))
-            # After books have been presented, use page_count with these books to count pages, return int to display
-        elif menu_input == "c" or menu_input == "C":
-            print("1")
-            # List completed books
+        if menu_input == "r" or menu_input == "R": # TODO: "Required books:" or "None"
+            page_sum = 0  # gotta be a better way to count pages
+            num_books = 0  # this could be in a list - make required reading into a list and return that from display books so can be used for "Required Books" and to easily count how many required books there are. You can return it back to the main list when done so changes are always accounted for, or maybe just do that when marking/adding a book.
+            display_book(book_list, menu_input, page_sum, num_books)
+        elif menu_input == "c" or menu_input == "C": # TODO: "Completed books:" or "None"
+            page_sum = 0
+            num_books = 0
+            display_book(book_list, menu_input, page_sum, num_books)
         elif menu_input == "a" or menu_input == "A":
             print("2")
             # Add new book
@@ -47,15 +42,30 @@ def load_book(book_list, file):
         book_list.append(line.split(","))
     return book_list
 
+# (rename func) or (isolate page_sum into own func and call from main)
+def display_book(book_list, menu_input, pages, num_books):
+    for selection, i in enumerate(book_list):
+        if menu_input == "r":
+            if i[-1] == "r\n":
+                print("{:>3}. {:<40} {:<20} {} pages".format(selection, (i[0]), (i[1]), (i[2])))
+                pages += int(i[2])
+                num_books += 1
+        if menu_input == "c":
+            if i[-1] == "c\n":
+                print("{:>3}. {:<40} {:<20} {} pages".format(selection, (i[0]), (i[1]), (i[2])))
+                pages += int(i[2])
+                num_books += 1
+    page_sum(num_books, pages)
 
-def display_book(i, selection):
-    if i[-1] == "r\n":
-        return print("{:>3}. {:<40} {:<20} {} pages".format(selection, (i[0]), (i[1]), (i[2])))
-        #call page nubmer function with i and selection parameters, then make a list in page nubmers to forward that page number to main
-            #only used in required and completed
+
+# Neccessary? Would it be better suited if called from main?
+def page_sum(num_books, page_sum):
+    print("Total pages for {} books: {}".format(num_books, page_sum))
 
 
-# def page_count():
+#returns a list that'll be appended to book_list when called as . . .
+def add_book():
+    print("ok")
 
 
 if __name__ == '__main__':
