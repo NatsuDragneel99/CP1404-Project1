f54ea4f9de371667912f5e29f313cadb349366d0
BroderickThomsen <broderickwsthomsen@gmail.com>
2017-04-02 00:17:26 +1000

    Add complete_book function, remove book_check function

--- assignment1.py
+++ assignment1.py
@@ -16,20 +16,24 @@ def main():
         print("Menu:\nR - List required books\nC - List completed books\nA - Add new book\nM - Mark a book as completed\nQ - Quit")
         menu_input = str(input(">>> "))
         if menu_input == "r" or menu_input == "R":
+            print("Required books:")
             display_book(book_list, menu_input)
         elif menu_input == "c" or menu_input == "C":
+            print("Completed books:")
             display_book(book_list, menu_input)
         elif menu_input == "a" or menu_input == "A":
             book_list = book_list + [add_book()]
             print("{} by {}, ({} pages) added to reading list".format(book_list[-1][0], book_list[-1][1], book_list[-1][2]))
         elif menu_input == "m" or menu_input == "M":
+            print("Required books:")
             display_book(book_list, menu_input)
-            # Mark a book as completed
         elif menu_input == "q" or menu_input == "Q":
             program_loop = 0
-            # Quit
+            # Save changes to file
+            # X books saved to file.csv
         else:
             print("Invalid menu choice")
+    print("Have a nice day :)")
 
 
 def load_book(book_list, file):
@@ -38,55 +42,42 @@ def load_book(book_list, file):
     return book_list
 
 
-def book_check(book_list, menu_input):
-    x = 0
-    y = 0
-    for selection, i in enumerate(book_list):
-        if i[-1] == "r\n":
-            x = 1
-        elif i[-1] == "c\n":
-            y = 1
-    if menu_input == "r" or menu_input == "R" or menu_input == "m" or menu_input == "M":
-        print("Required books: ")
-        if x == 0:
-            print("No books")
-        return x
-    elif menu_input == "c" or menu_input == "C":
-        print("Completed books: ")
-        if y == 0:
-            print("No books")
-        return y
-
-
-# (rename func) or (isolate page_sum into own func and call from main)
+# rename function?
 def display_book(book_list, menu_input):
-    b = book_check(book_list, menu_input)
+    y = 0
     pages = 0
     books = 0
     for selection, i in enumerate(book_list):
         if menu_input == "r" or menu_input == "R" or menu_input == "m" or menu_input == "M":
             if i[-1] == "r\n":
-                print("{:>3}. {:<40} {:<20} {} pages".format(selection, (i[0]), (i[1]), (i[2])))
+                print("{:>3}. {:<40} {:<20} {} pages".format(selection, (i[0]), (i[1]), (i[2]))) #turn into function
                 pages += int(i[2])
                 books += 1
+                y = 1
         elif menu_input == "c" or menu_input == "C":
             if i[-1] == "c\n":
                 print("{:>3}. {:<40} {:<20} {} pages".format(selection, (i[0]), (i[1]), (i[2])))
                 pages += int(i[2])
                 books += 1
-    page_sum(books, pages, b)
+                y = 1
+    if y == 0:
+        print("No books")
+    page_sum(books, pages, y)
+    if menu_input == "m" or menu_input == "M":
+        if y == 1:
+            complete_book(book_list)
 
 
 # Necessary? Would it be better suited if called from main, to justify existence?
-def page_sum(num_books, total_pages, b):
-    if b == 1:
+def page_sum(num_books, total_pages, y):
+    if y == 1:
         print("Total pages for {} books: {}".format(num_books, total_pages))
 
 
 # returns a list that'll be appended to book_list when called as . . .
 def add_book():
     loop = 1
-    title = str # consider inclusion
+    title = str
     author = str
     pages = int
     while loop != 0:
@@ -116,8 +107,22 @@ def add_book():
     return new_book
 
 
-def complete_book():
-    print ("ok")
+def complete_book(book_list):
+    loop = 1
+    x = -1
+    print("Enter the number of a book to mark as completed")
+    while loop != 0:
+        try:
+            x = int(input(">>> "))
+            if book_list[x][-1] == "c\n":
+                print("That book is already completed")
+                return
+            elif x >= 0 and book_list[x][-1] != "c\n":
+                book_list[x][-1] = "c\n"
+                print("{} by {} marked as completed".format(book_list[x][0], book_list[x][1]))
+                return book_list
+        except (ValueError, IndexError):
+            print("Invalid input; enter a valid number")
 
 
 if __name__ == '__main__':
