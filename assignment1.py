a386c823328b291dd8dba9d8f87c74135d59b804
BroderickThomsen <broderickwsthomsen@gmail.com>
2017-04-06 03:24:34 +1000

    Rewrite complete_book function and add save_books function

--- assignment1.py
+++ assignment1.py
@@ -21,15 +21,15 @@ def main():
         if menu_input == "r":
             if book_check(books_list, menu_input):
                 print("Required books:")
+                display_books(books_list, menu_input)
             else:
                 print("No books")
-
         elif menu_input == "c":
             if book_check(books_list, menu_input):
                 print("Completed books:")
+                display_books(books_list, menu_input)
             else:
                 print("No books")
-
         elif menu_input == "a":
             books_list = books_list + [add_book()]
             print("{} by {}, ({} pages) added to reading list".format(books_list[-1][0], books_list[-1][1],
@@ -37,34 +37,17 @@ def main():
         elif menu_input == "m":
             if book_check(books_list, menu_input):
                 print("Required books:")
+                display_books(books_list, menu_input)
+                complete_book(books_list)
             else:
                 print("No required books")
         else:
             print("Invalid menu choice")
         menu_input = str(input("Menu:\nR - List required books\nC - List completed books\nA - Add new book\n"
                                "M - Mark a book as completed\nQ - Quit\n>>>")).lower()
-    # def save_books():
-    file = open("books.csv", "w")
-    books_string = ""  # swap  to old method of just writing to file from str(books_list), more efficient, less complex
-    for book in books_list:
-        book_string = ",".join(book)
-        books_string += book_string
-    file.seek(0)
-    file.write(books_string)
-    file.close()
-
+    save_books(books_list)
     print("{} books saved to {}".format(len(books_list), FILE_NAME))
     print("Have a nice day :)")
-
-
-def book_check(books_list, mode):
-    book_status = False
-    for book in books_list:
-        if book[-1] == "r\n" and mode == "r" or mode == "m":
-            book_status = True
-        elif book[-1] == "c\n" and mode == "c":
-            book_status = True
-    return book_status
 
 
 def load_book():
@@ -76,51 +59,30 @@ def load_book():
     return book_list
 
 
-def display_books(books_list, mode):  # consider renaming function (print_book), parameters
-    list_content = False
+def book_check(books_list, mode):
+    book_status = False
+    for book in books_list:
+        if book[-1] == "r\n" and mode == "r" or mode == "m":
+            book_status = True
+        elif book[-1] == "c\n" and mode == "c":
+            book_status = True
+    return book_status
+
+def display_books(books_list, mode):
     num_pages = 0
     num_books = 0
     for book, item in enumerate(books_list):
-        if item[-1] != "r\n" and mode == "r" or mode == "m":
-            continue
+        if item[-1] != "r\n":
+            if mode == "r" or mode == "m":
+                continue
         elif item[-1] != "c\n" and mode == "c":
             continue
         print("{:>3}. {:<40} {:<20} {} pages".format(book, (item[0]), (item[1]), item[2]))
         num_pages += int(item[2])
         num_books += 1
-        list_content = True
-    if not list_content:
-        print("No books")
     print("Total pages for {} books: {}".format(num_books, num_pages))
 
 
-# def display_books(books_list, mode):  # consider renaming function (print_book), parameters
-#     list_content = False  # consider renaming
-#     num_pages = 0
-#     num_books = 0
-#     for book, item in enumerate(books_list):  # consider renaming indexes
-#         if item[-1] == "r\n" and mode == "r" or mode == "m":
-#             print("{:>3}. {:<40} {:<20} {} pages".format(book, (item[0]), (item[1]), item[2]))
-#             num_pages += int(item[2])
-#             num_books += 1
-#             list_content = True
-#         if item[-1] == "c\n" and mode == "c":
-#             print("{:>3}. {:<40} {:<20} {} pages".format(book, (item[0]), (item[1]), (item[2])))
-#             num_pages += int(item[2])
-#             num_books += 1
-#             list_content = True
-#     if not list_content:
-#         print("No books")
-#     print("Total pages for {} books: {}".format(num_books, num_pages))
-
-
-#
-#     if mode == "m" or mode == "M":
-#         if book_check == 1:
-#             complete_book(book_list)
-
-
-# returns a list that'll be appended to book_list when called as . . .
 def add_book():
     loop = 1  # consider renaming
     title = str
@@ -154,21 +116,34 @@ def add_book():
     return new_book
 
 
-def complete_book(book_list):
-    loop = 1
+def complete_book(books_list):
+    loop = True
     print("Enter the number of a book to mark as completed")
-    while loop != 0:
+    while loop:
         try:
-            marking_input = int(input(">>> "))  # consider renaming
-            if book_list[marking_input][-1] == "c\n":
+            mark_choice = int(input(">>> "))
+            if mark_choice >= 0 and books_list[mark_choice][-1] != "c\n":
+                books_list[mark_choice][-1] = "c\n"
+                print("{} by {} marked as completed".format(books_list[mark_choice][0], books_list[mark_choice][1]))
+                return books_list
+            elif books_list[mark_choice][-1] == "c\n":
                 print("That book is already completed")
-                return
-            elif marking_input >= 0 and book_list[marking_input][-1] != "c\n":
-                book_list[marking_input][-1] = "c\n"
-                print("{} by {} marked as completed".format(book_list[marking_input][0], book_list[marking_input][1]))
-                return book_list
+                loop = False
+            else:
+                print("Invalid input; enter a valid number")
         except (ValueError, IndexError):
             print("Invalid input; enter a valid number")
+
+
+def save_books(books_list):
+    file = open("books.csv", "w")
+    books_string = ""  # swap  to old method of just writing to file from str(books_list), more efficient, less complex
+    for book in books_list:
+        book_string = ",".join(book)
+        books_string += book_string
+    file.seek(0)
+    file.write(books_string)
+    file.close()
 
 
 if __name__ == '__main__':
